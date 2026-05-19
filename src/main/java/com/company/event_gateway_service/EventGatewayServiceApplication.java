ab8ee8151b8a7566d19715b7f4016af4955bec42
StefanDeveloper <belciugstefan@gmail.com>
2026-05-19 16:54:08 +0100

    PA-38 -initial commit

---
+++ src/main/java/com/company/event_gateway_service/EventGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.event_gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/event_gateway_service/EventGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.event_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
