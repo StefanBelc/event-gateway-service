bfa2fa14b7bd88629572ae4ac5757a0310ded4a4
StefanDeveloper <belciugstefan@gmail.com>
2026-06-10 10:12:15 +0100

    PA-38 - event subscribers

---
+++ src/main/java/com/company/event_gateway_service/kafka/subscriber/GameEventSubscriber.java
@@ -0,0 +1,20 @@
+package com.company.event_gateway_service.kafka.subscriber;
+
+import com.company.event_gateway_service.event.GameEvent;
+import com.company.event_gateway_service.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class GameEventSubscriber {
+
+    private final NotificationService notificationService;
+
+    @KafkaListener(topics = "${myapp.kafka.topics.game-events}", groupId = "gateway-service" )
+    public void handleGameEvent(GameEvent gameEvent) {
+        notificationService.broadcastEvent(gameEvent);
+    }
+
+}
